2ac6d2a2e1e0ae1bdb2bd03f6ea8546c0a899895
root <root@jenkins.linux.com>
2018-05-30 18:35:38 -0400

    adding ELB/modifications for ELB

--- main.tf
+++ main.tf
@@ -36,6 +36,10 @@ resource "aws_launch_configuration" "example" {
 resource "aws_autoscaling_group" "example" {
   launch_configuration = "${aws_launch_configuration.example.id}"
   availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+  load_balancers = ["${aws_elb.example.name}"]
+  health_check_type = "ELB"
+
   max_size = 10
   min_size = 2
 
@@ -44,4 +48,51 @@ resource "aws_autoscaling_group" "example" {
     propagate_at_launch = true
     value = "terraform-asg-example"
   }
+}
+
+
+resource "aws_elb" "example" {
+  name = "terraform-asg-example"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  security_groups = ["${aws_security_group.elb.id}"]
+
+  "listener" {
+    instance_port = "${var.server_port}"
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+
+  # health_check block that sends an HTTP request every 30 seconds to the “/” URL of each of the EC2 Instances in the ASG and only considers an Instance healthy if it responds with a 200 OK
+  health_check {
+    healthy_threshold = 2
+    interval = 30
+    target = "HTTP:${var.server_port}/"
+    timeout = 3
+    unhealthy_threshold = 2
+  }
+}
+
+
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
+output "elb_dns_name" {
+  value = "${aws_elb}"
 }
